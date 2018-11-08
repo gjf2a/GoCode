5fb4ea921be71873ff06b4ad3f51243b0c77c462
Gabriel Ferrer <ferrer@hendrix.edu>
2018-11-07 20:42:28 -0600

    Successful video capture.

--- app/src/main/java/com/stack/gocode/primaryFragments/VideoRecordingFragment.java
+++ app/src/main/java/com/stack/gocode/primaryFragments/VideoRecordingFragment.java
@@ -3,14 +3,19 @@ package com.stack.gocode.primaryFragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.stack.gocode.MainActivity;
 import com.stack.gocode.R;
 
+import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
 /**
@@ -18,9 +23,13 @@ import org.opencv.core.Mat;
  */
 
 public class VideoRecordingFragment extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2 {
+    public final static String TAG = VideoRecordingFragment.class.getSimpleName();
+
     private View myView;
 
     private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback mLoaderCallback;
 
     @Nullable
     @Override
@@ -30,6 +39,24 @@ public class VideoRecordingFragment extends Fragment implements CameraBridgeView
         mOpenCvCameraView = (CameraBridgeViewBase) myView.findViewById(R.id.video_record_surface_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        mLoaderCallback = new BaseLoaderCallback(myView.getContext()) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case LoaderCallbackInterface.SUCCESS:
+                    {
+                        Log.i(TAG, "OpenCV loaded successfully");
+                        mOpenCvCameraView.enableView();
+                    } break;
+                    default:
+                    {
+                        super.onManagerConnected(status);
+                    } break;
+                }
+            }
+        };
+
         return myView;
     }
 
@@ -46,5 +73,18 @@ public class VideoRecordingFragment extends Fragment implements CameraBridgeView
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         return inputFrame.rgba();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, myView.getContext(), mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
     }
 }
